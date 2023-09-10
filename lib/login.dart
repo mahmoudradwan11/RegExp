a24667e504b1fb3479cf0e90702021322fa993ef
mahmoudradwan11 <102020555+mahmoudradwan11@users.noreply.github.com>
2023-09-10 19:03:26 +0300

    Apply RegExp

---
+++ lib/login.dart
@@ -0,0 +1,187 @@
+import 'package:exp/exp.dart';
+import 'package:exp/home.dart';
+import 'package:exp/widgets.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  var formKey = GlobalKey<FormState>();
+  var emailController = TextEditingController();
+  var phoneController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 80),
+                  const Text(
+                    'Welcome!',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Please enter your data to continue',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    height: 70,
+                  ),
+                  const Align(
+                      alignment: AlignmentDirectional.topStart,
+                      child: Text(
+                        'UserEmail',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 15),
+                      )),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      //color: HexColor('#F2F2F2'),
+                    ),
+                    child: DefaultFieldForm(
+                        controller: emailController,
+                        keyboard: TextInputType.emailAddress,
+                        valid: (value) {
+                          if (value.isEmpty) {
+                            return 'Please Enter Your Email';
+                          }
+                          return null;
+                        },
+                        prefix: Icons.email,
+                        hint: 'Email',
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        show: false),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Align(
+                      alignment: AlignmentDirectional.topStart,
+                      child: Text(
+                        'Password',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 15),
+                      )),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      //color: HexColor('#F2F2F2'),
+                    ),
+                    child: DefaultFieldForm(
+                        controller: phoneController,
+                        keyboard: TextInputType.emailAddress,
+                        valid: (value) {
+                          if (value.isEmpty) {
+                            return 'Please Enter Your Phone';
+                          }
+                          return null;
+                        },
+                        prefix: Icons.lock,
+                        hint: 'Phone',
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        show: false,
+                        suffix: Icons.visibility,
+                        suffixPress: () {
+                          //cubit.changePasswordIcon();
+                        }),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50, right: 50),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: 1,
+                          width: 100,
+                          color: Colors.black,
+                        ),
+                        const Text(
+                          'Or',
+                          style: TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                        Container(
+                          height: 1,
+                          width: 100,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  DefaultButton(
+                      //backgroundColor: HexColor('#07094D'),
+                      buttonWidget: const Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      function: () {
+                        if (formKey.currentState!.validate()){
+                          if (Exp.isEmailValid(emailController.text)){
+                            print('Email Is Valid');
+                          }else{
+                            print('Email Is InValid');
+                          }
+                          if(Exp.isPhoneValid(phoneController.text)){
+    print('phone Is Valid');
+    }else{
+    print('Phone Is InValid');
+    }}
+                        if(Exp.isEmailValid(emailController.text)&&(Exp.isPhoneValid(phoneController.text))){
+                          navigateTo(context,const MyHomePage(title:'Exp Flutter'));
+                        }
+                      }),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30, top: 10),
+                    child: Row(
+                      children: [
+                        const Text('I don\'t Have account?'),
+                        MaterialButton(
+                            onPressed: () {
+                              // navigateTo(context, Register());
+                            },
+                            child: Text(
+                              'Create Account',
+                            ))
+                        //style: TextStyle(color: HexColor('#07094D'),),))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/widgets.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+class DefaultButton extends StatelessWidget {
+  Widget buttonWidget;
+  Function() function;
+  double width;
+  Color backgroundColor;
+  bool isUpperCase;
+  double radius;
+  double height;
+  Color borderColor;
+  DefaultButton({
+    super.key,
+    this.height = 50,
+    required this.buttonWidget,
+    required this.function,
+    this.width = double.infinity,
+    this.backgroundColor = Colors.blue,
+    this.isUpperCase = true,
+    this.radius = 10.0,
+    this.borderColor = Colors.black,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        border: Border.all(color: borderColor),
+        borderRadius: BorderRadius.circular(radius),
+        color: backgroundColor,
+      ),
+      child: MaterialButton(
+        onPressed: function,
+        child: buttonWidget,
+      ),
+    );
+  }
+}
+class DefaultFieldForm extends StatelessWidget {
+  TextEditingController controller;
+  TextInputType keyboard;
+  var valid;
+  String? label;
+  String? hint;
+  IconData? prefix;
+  bool show;
+  var tap;
+  var onchange;
+  var onSubmit;
+  IconData? suffix;
+  Function()? suffixPress;
+  TextStyle? labelStyle;
+  TextStyle? hintStyle;
+
+  DefaultFieldForm({
+    super.key,
+    required this.controller,
+    required this.keyboard,
+    required this.valid,
+    this.hint,
+    this.label,
+    this.prefix,
+    this.show = false,
+    this.tap,
+    this.onchange,
+    this.onSubmit,
+    this.suffix,
+    this.suffixPress,
+    this.labelStyle,
+    this.hintStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onTapOutside: (event)=>FocusScope.of(context).unfocus(),
+      validator: valid,
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: hintStyle,
+        labelText: label,
+        labelStyle: labelStyle,
+        contentPadding: const EdgeInsets.all(16),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(color: Colors.indigo),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(color: Colors.black),
+        ),
+        errorStyle:const TextStyle(color: Colors.red,fontSize: 15),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(color: Colors.black),
+        ),
+        // borderRadius: BorderRadius.circular(30
+        prefixIcon: Icon(
+          prefix,
+          color: Colors.grey[500],
+        ),
+        suffixIcon: suffix != null
+            ? IconButton(
+          icon: Icon(suffix),
+          onPressed: suffixPress,
+          color: Colors.grey[500],
+        )
+            : null,
+      ),
+
+      keyboardType: keyboard,
+      onFieldSubmitted: onSubmit,
+      onChanged:onchange,
+      obscureText: show,
+      onTap: tap,
+    );
+  }
+}
+void navigateTo(context, widget) =>
+    Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
+
+void navigateAndFinish(context, widget) => Navigator.pushAndRemoveUntil(
+  context,
+  MaterialPageRoute(builder: (context) => widget),
+      (Route<dynamic> route) => false,
+);
+void navigateAndReplace(context, widget) => Navigator.pushReplacement(
+    context, MaterialPageRoute(builder: (context) => widget));
+
